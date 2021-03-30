2aa4a965fb3ce714d579e8f5104c9c437886d48a
BriceDon <65740750+boodscode237@users.noreply.github.com>
2021-03-30 23:21:36 +0500

    Fetch api

---
+++ fetch_api/app.js
@@ -0,0 +1,9 @@
+// FETCH API
+fetch("json/luigi.json").then((res) => {
+    console.log(res)
+    return res.json() //make our response usable it = to json.parse
+}).then(data => {
+    console.log(data)
+}).catch(e => {
+    console.log(e)
+})
