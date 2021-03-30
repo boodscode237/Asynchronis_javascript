8c540d71afcfd60b2f0aa3aaab883c744332e756
BriceDon <65740750+boodscode237@users.noreply.github.com>
2021-03-30 20:29:26 +0500

    first commit

---
+++ httpRequests/app.js
@@ -0,0 +1,24 @@
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            callback(undefined, request.responseText)
+        } else if (request.readyState === 4) {
+            callback('could not find', undefined)
+        }
+    })
+    request.open('GET', "https://jsonplaceholder.typicode.com/todos/")
+    request.send()
+}
+console.log(1)
+console.log(2)
+getTodos((err, data) => {
+    console.log("object")
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+    }
+})
+console.log(3)
+console.log(4)
